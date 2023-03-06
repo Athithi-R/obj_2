12c569dc1d413e156582ecedb22022208279b756
Athithi-R <93078294+Athithi-R@users.noreply.github.com>
2023-03-06 21:08:37 +0530

    Add files via upload

---
+++ living.js
@@ -0,0 +1,50 @@
+ status="";
+ img="";
+
+ function mainpg(){
+    window.location="index.html";
+ }
+
+ function preload(){
+   img=loadImage('livingroom.jpg');
+   
+ }
+
+ function setup(){
+    canvas=createCanvas(640,420);
+    canvas.center();
+    canvas.position(450,220);
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+   document.getElementById("status").innerHTML = "Status : Detecting Objects";
+
+ }
+ function modelLoaded() {
+   console.log("Model Loaded!")
+   status = true;
+   objectDetector.detect(img, gotResult);
+ }
+ function draw(){
+    image(img,0,0,640,420);
+ }
+ function gotResult(error, results) {
+   if (error) {
+     console.log(error);
+   }
+   console.log(results);
+ }
+ 
+ 
+ function draw() {
+   image(img, 0, 0, 640, 420);
+   
+ 
+   fill("#FF0000");
+   text("chair", 190, 70);
+   noFill();
+   stroke("#FF0000");
+   rect(170, 50, 250, 240 );
+ }
+ 
+
+ 
+
